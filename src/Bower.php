a9ed88a1125ade885b8a63901abee5f44310be02
devLegend <dev.lllypyn@gmail.com>
2016-02-23 11:59:06 +0300

    updated docs

--- src/Bower.php
+++ src/Bower.php
@@ -20,10 +20,8 @@ class Bower implements Plugin
     /**
      * Standard Constructor
      *
-     * $options['directory'] Output Directory. Default: %BUILDPATH%
-     * $options['filename']  Phar Filename. Default: build.phar
-     * $options['regexp']    Regular Expression Filename Capture. Default: /\.php$/
-     * $options['stub']      Stub Content. No Default Value
+     * $options['command']
+     * $options['flags']
      *
      * @param Builder $phpci
      * @param Build $build
